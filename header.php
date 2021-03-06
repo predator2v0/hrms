81b4d0b72c69f9d430b236fd86547b8768dc4cff
subhakanta roul <papu.roul99@gmail.com>
2021-03-06 20:05:31 +0530

    common header for all pages

---
+++ header.php
@@ -0,0 +1,70 @@
+        <!-- welcome section begins -->
+        <section class="welcome-section">
+            <header>
+                <!-- navbar section begins -->
+                <nav class="navbar navbar-expand-md">
+                    <div class="container">
+                        <a
+                            href="#"
+                            class="navbar-brand text-dark font-weight-bold"
+                            >CustomCastle</a
+                        >
+                        <button
+                            class="navbar-toggler"
+                            type="button"
+                            data-toggle="collapse"
+                            data-target="#collapsenav"
+                        >
+                            <span>
+                                <img
+                                    src="img/menuHamburger.svg"
+                                    width="35px"
+                                    height="35px"
+                                    alt="menu-hamberger-icon"
+                                />
+                            </span>
+                        </button>
+                        <div
+                            class="collapse navbar-collapse text-center"
+                            id="collapsenav"
+                        >
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item">
+                                    <a href="#home" class="nav-link text-dark"
+                                        >home</a
+                                    >
+                                </li>
+                                <li class="nav-item">
+                                    <a
+                                        href="#about_us"
+                                        class="nav-link text-dark"
+                                        >about us</a
+                                    >
+                                </li>
+                                <li class="nav-item">
+                                    <a
+                                        href="#contact_us"
+                                        class="nav-link text-dark"
+                                        >contact us</a
+                                    >
+                                </li>
+                                <!-- <li class="nav-item">
+                                    <a href="#login" class="nav-link text-dark"
+                                        >login</a
+                                    >
+                                </li> -->
+                                <li class="nav-item signup">
+                                    <a
+                                        href="#register"
+                                        class="nav-link text-dark"
+                                        >register</a
+                                    >
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <!-- navbar ends -->
+            </header>
+            
+        </section>
