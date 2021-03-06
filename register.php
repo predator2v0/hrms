2947f71f675c018dd23ab64490aaa01548e65ecd
subhakanta roul <papu.roul99@gmail.com>
2021-03-06 20:06:32 +0530

    new user registration form

---
+++ register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>register | hrms</title>
+    <?php include 'links.php'?>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/register.css">
+    <link rel="shortcut icon" href="img/icon.png" type="image/x-icon">
+
+</head>
+<body>
+<?php include 'header.php'?>
+<section class="registration-page-container">
+    <div class="registration-form-container">
+        
+    </div>
+</section>
+<?php include 'footer.php'?>
+</body>
+</html>
