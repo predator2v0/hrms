b7eceb4cb536780188946a06926ecaa860d96ea8
subhakanta roul <papu.roul99@gmail.com>
2021-03-06 20:05:49 +0530

    common footer for all pages

---
+++ footer.php
@@ -0,0 +1,14 @@
+<section class="footer">
+            <h1 class="footer-heading">custom castle</h1>
+            <p class="github-repo">
+                <img
+                    class="github-icon"
+                    src="img/github-ico.png"
+                    alt="github-icon"
+                />
+                <a href="https://github.com/predator2v0/hrms" target="_blank"
+                    >github repository</a
+                >
+            </p>
+            <p class="copyright">&copy;2021 all rights reserved.</p>
+        </section>
