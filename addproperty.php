6a67c83855d6a00cac0761a1d6c25774d92ac4dc
subhakanta roul <papu.roul99@gmail.com>
2021-03-14 16:33:09 +0530

    form interface updated

--- addproperty.php
+++ addproperty.php
@@ -82,22 +82,99 @@
         </nav>
         <!-- navbar ends -->
     </header>
+    <!-- property registration form backend script [php] -->
+    <?php
+        include 'connection.php';
+
+        if($con){
+            if(isset($_POST['submit'])){
+                
+            }
+        }
+    ?>
+    <!-- property registration form frontend -->
     <section class="addproperty-container">
         <h1 class="welcome-heading">hello, <?php echo $_SESSION['name']; ?>!</h1>
         <hr>
         <div class="addproperty-form">
-            <form action="" method="POST">
+            <form action="" method="POST" enctype="multipart/form-data">
                 <div class="property-info">
-
+                    <h3>property information</h3>
+                    <div class="ipfield">
+                        <label>property name:</label>
+                        <input type="text" name="p_name" required>
+                    </div>
+                    <div class="ipfield">
+                        <label>locality:</label>
+                        <input type="text" name="p_locality" required>
+                    </div>
+                    <div class="ipfield">
+                        <label>landmark:</label>
+                        <input type="text" name="p_landmark" required>
+                    </div>
+                    <div class="ipfield">
+                        <label>city:</label>
+                        <input type="text" name="p_city" required>
+                    </div>
+                    <div class="ipfield">
+                        <label>state:</label>
+                        <input type="text" name="p_state" required>
+                    </div>
+                    <div class="ipfield">
+                        <label>pincode:</label>
+                        <input type="text" name="p_pincode" required>
+                    </div>
+                    <div class="ipfield">
+                        <label>registration type:</label>
+                        <select name="p_regtype" id="">
+                            <option value="">--select--</option>
+                            <option value="apartment">apartment</option>
+                            <option value="hostel">hostel</option>
+                            <option value="house">house</option>
+                            <option value="office">office</option>
+                            <option value="shop">shop</option>
+                        </select>
+                    </div>
+                    <div class="ipfield">
+                        <label>property type:</label>
+                        <select name="p_type" id="">
+                            <option value="">--select--</option>
+                            <option value="onebhk">1 BHK</option>
+                            <option value="twobhk">2 BHK</option>
+                            <option value="threebhk">3 BHK</option>
+                        </select>
+                    </div>
+                    
                 </div>
-                <div class="contact-info">
-
+                <div class="owner-info">
+                    <h3>owner information</h3>
+                    <div class="ipfield">
+                        <label>name:</label>
+                        <input type="text" name="o_name" value="<?php echo $_SESSION['name']; ?>" required>
+                    </div>
+                    <div class="ipfield">
+                        <label>email:</label>
+                        <input type="email" name="o_email" value="<?php echo $_SESSION['email']; ?>" required disabled>
+                    </div>
+                    <div class="ipfield">
+                        <label>contact no:</label>
+                        <input type="text" name="o_contact" value="<?php echo $_SESSION['contact']; ?>" required>
+                    </div>
                 </div>
                 <div class="property-image">
-
+                    <h3>upload image</h3>
+                    <div class="ipfield">
+                        <label>image 1:</label>
+                        <input class="img-upload" type="file" name="p_pic1" required>
+                    </div>
+                    <div class="ipfield">
+                        <label>image 2:</label>
+                        <input class="img-upload" type="file" name="p_pic2" required>
+                    </div>
                 </div>
                 <div class="submit-reset">
-
+                <input type="submit" name="submit" value="register">
+                <input type="reset" name="reset">
                 </div>
             </form>
         </div>
