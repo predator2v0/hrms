0f51ddd5e60ab68b4bce2f4800a096cabda7bfe0
subhakanta roul <papu.roul99@gmail.com>
2021-04-09 15:18:05 +0530

    view property page added

---
+++ viewproperty.php
@@ -0,0 +1,122 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Property | hrms</title>
+    <?php include 'links.php'; ?>
+    <link rel="shortcut icon" href="img/icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/viewproperty.css">
+</head>
+<body class="viewproperty-body">
+<header>
+        <!-- navbar section begins -->
+        <nav class="navbar navbar-expand-md">
+            <div class="container">
+                <a
+                    href="index.html"
+                    class="navbar-brand text-dark font-weight-bold"
+                    >CustomCastle</a
+                >
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#collapsenav"
+                >
+                    <span>
+                        <img
+                            src="img/menuHamburger.svg"
+                            width="35px"
+                            height="35px"
+                            alt="menu-hamberger-icon"
+                        />
+                    </span>
+                </button>
+                <div
+                    class="collapse navbar-collapse text-center"
+                    id="collapsenav"
+                >
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a href="index.html" class="nav-link text-dark"
+                                >home</a
+                            >
+                        </li>
+                        <!-- <li class="nav-item">
+                            <a
+                                href="#about_us"
+                                class="nav-link text-dark"
+                                >about us</a
+                            >
+                        </li>
+                        <li class="nav-item">
+                            <a
+                                href="#contact_us"
+                                class="nav-link text-dark"
+                                >contact us</a
+                            >
+                        </li>-->
+                        <!-- <li class="nav-item">
+                            <a
+                                href="login.php"
+                                class="nav-link text-dark"
+                                >HELLO </a
+                            >
+                        </li> -->
+                        <li class="nav-item signup">
+                            <a
+                                href="logout.php"
+                                class="nav-link text-dark"
+                                >logout</a
+                            >
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- navbar ends -->
+    </header>
+    
+    <section class="viewproperty-container">
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <td>sl. no.</td>
+                    <td>property name</td>
+                    <td>address</td>
+                    <td>landmark</td>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+                include 'connection.php';
+                if($_SESSION['name']){
+                $mail = $_SESSION['email'];
+                $retrive_query = "select * from property where oemail = '$mail' ";
+                $resultset = mysqli_query($con,$retrive_query);
+                $rows = mysqli_num_rows($resultset);
+                while($res = mysqli_fetch_array($resultset)){
+            ?>
+                <tr>
+                    <td><?php echo $res['id']; ?></td>
+                    <td><?php echo $res['pname']; ?></td>
+                    <td><?php echo $res['locality']; ?></td>
+                    <td><?php echo $res['landmark']; ?></td>
+                </tr>
+            <?php
+                }
+            }
+            ?>  
+            </tbody>
+        </table>
+    </section>
+
+    <?php include 'footer.php'; ?>
+</body>
+</html>
