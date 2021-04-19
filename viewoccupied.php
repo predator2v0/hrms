c8c3579ca038bc6a5b1261f8119970690cfc09ef
subhakanta roul <papu.roul99@gmail.com>
2021-04-20 01:22:54 +0530

    page to display the occupied properties by a user

---
+++ viewoccupied.php
@@ -0,0 +1,156 @@
+<?php
+    session_start();
+
+    if(!isset($_SESSION['email'])){
+        header('location:login.php');
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>occupied property | hrms</title>
+    <?php include 'links.php'; ?>
+    <link rel="shortcut icon" href="img/icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/viewproperty.css">
+</head>
+<body class="viewproperty-body">
+<header>
+        <!-- navbar section begins -->
+        <nav class="navbar navbar-expand-md">
+            <div class="container">
+                <a
+                    href="index.php"
+                    class="navbar-brand text-dark font-weight-bold"
+                    >CustomCastle</a
+                >
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#collapsenav"
+                >
+                    <span>
+                        <img
+                            src="img/menuHamburger.svg"
+                            width="35px"
+                            height="35px"
+                            alt="menu-hamberger-icon"
+                        />
+                    </span>
+                </button>
+                <div
+                    class="collapse navbar-collapse text-center"
+                    id="collapsenav"
+                >
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a href="index.php" class="nav-link text-dark"
+                                >home</a
+                            >
+                        </li>
+                        <!-- <li class="nav-item">
+                            <a
+                                href="#about_us"
+                                class="nav-link text-dark"
+                                >about us</a
+                            >
+                        </li>
+                        <li class="nav-item">
+                            <a
+                                href="#contact_us"
+                                class="nav-link text-dark"
+                                >contact us</a
+                            >
+                        </li>-->
+                        <li class="nav-item">
+                            <a
+                                href="dashboard.php"
+                                class="nav-link text-dark"
+                                >dashboard </a
+                            >
+                        </li> 
+                        <li class="nav-item signup">
+                            <a
+                                href="logout.php"
+                                class="nav-link text-dark"
+                                >logout</a
+                            >
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- navbar ends -->
+    </header>
+    
+    <section class="viewproperty-container">
+        <table class="table table-striped table-hover table-bordered">
+            <thead class="font-weight-bold">
+                <tr class="text-uppercase">
+                    <td>sl. no.</td>
+                    <td>property name</td>
+                    <td>address</td>
+                    <td>landmark</td>
+                    <td>city</td>
+                    <td>state</td>
+                    <td>pincode</td>
+                    <td>type</td>
+                    <td>rooms</td>
+                    <td>owner email</td>
+                    <td>owner contact</td>
+                    <!-- <td colspan="2">Operations</td> -->
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+                include 'connection.php';
+                if($_SESSION['name']){
+                $mail = $_SESSION['email'];
+                
+                $retrive_query = "select * from property where temail = '$mail' ";
+                $resultset = mysqli_query($con,$retrive_query);
+                $rows = mysqli_num_rows($resultset);
+                $sl = 1;
+                while($res = mysqli_fetch_array($resultset)){
+            ?>
+                <tr>
+                    <td><?php echo $sl; ?></td>
+                    <td><?php echo $res['pname']; ?></td>
+                    <td><?php echo $res['locality']; ?></td>
+                    <td><?php echo $res['landmark']; ?></td>
+                    <td><?php echo $res['city']; ?></td>
+                    <td><?php echo $res['state']; ?></td>
+                    <td><?php echo $res['pincode']; ?></td>
+                    <td><?php echo $res['regtype']; ?></td>
+                    <td><?php 
+                            if($res['roomcount'] == 'onebhk')
+                                echo "1 BHK";
+                            if($res['roomcount'] == 'twobhk')
+                                echo "2 BHK";
+                            if($res['roomcount'] == 'threebhk')
+                                echo "3 BHK";
+                            
+                        ?>
+                    </td>
+                    <td><?php echo $res['oemail']; ?></td>
+                    <td><?php echo $res['ocontact']; ?></td>
+                    <!-- <td><a href="updateproperty.php?id=<?php echo $res['id']; ?>" data-toggle="tooltip" data-placement="right" title="Update"><i class="fa fa-edit text-success" aria-hidden="true"></i></a></td>
+                    <td><a href="deleteproperty.php?id=<?php echo $res['id']; ?>" data-toggle="tooltip" data-placement="left" title="Delete"><i class="fa fa-trash text-danger" aria-hidden="true"></i></a></td> -->
+                </tr>
+            <?php
+                $sl++;
+                }
+            }
+            ?>  
+            </tbody>
+        </table>
+    </section>
+
+    <?php include 'footer.php'; ?>
+    <script src="script/viewproperty.js"></script>
+</body>
+</html>
