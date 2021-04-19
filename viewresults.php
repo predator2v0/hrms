509ce4da422e9264e38eddcbccdf352b2eefff46
subhakanta roul <papu.roul99@gmail.com>
2021-04-19 22:13:52 +0530

    the view search results page optimized

--- viewresults.php
+++ viewresults.php
@@ -93,18 +93,69 @@
             if($con){
                 if(isset($_POST['submit'])){
                     $pcode = $_POST['pcode'];
+                    $fetch_query = " select * from property where pincode = '$pcode'";
+                    $resultset = mysqli_query($con,$fetch_query);
+                    $num = mysqli_num_rows($resultset);
+                    
+                    while($res = mysqli_fetch_assoc($resultset)){
+                     ?>
+                        <div class="result-box">
+                            <div class="property-img " >
+                                <img id="bimg1" class="img-fluid" src="<?php echo $res['pic1']; ?>" alt="">
+                            </div>
+                            <div class="property-details">
+                                <p>name: <span><?php echo $res['pname']; ?></span></p>
+                                <p>address: <span><?php echo $res['locality'].",". $res['landmark']; ?></span></p>
+                                <p>city/state: <span><?php echo $res['city'].",". $res['state']; ?></span></p>
+                                <p>type: 
+                                    <span>
+                                        <?php
+                                            if($res['roomcount'] == 'onebhk'){
+                                                echo $res['regtype'].", 1 BHK";
+                                            } 
+                                            if($res['roomcount'] == 'twobhk'){
+                                                echo $res['regtype'].", 2 BHK";
+                                            } 
+                                            if($res['roomcount'] == 'threebhk'){
+                                                echo $res['regtype'].", 3 BHK";
+                                            } 
+                                        ?>
+                                    </span>
+                                </p>
+                                <p>price range: <span><?php echo $res['minprice']." - ". $res['maxprice']; ?></span> </p>
+                                <p>rent status:
+                                    <span class="text-danger">
+                                        <?php 
+                                            if($res['temail'] != null){
+                                                echo "RENTED";
+                                            } else{
+                                                echo "NOT RENTED";
+                                            }
+                                        ?>
+                                    </span>
+                                </p>
+                            </div>
+                            <div class="owner-details">
+                                <p>owner name: <span><?php echo $res['oname']; ?></span></p>
+                                <p>email: <span class="text-lowercase"><?php echo $res['oemail']; ?></span></p>
+                                <p>contact: <span class="text-lowercase"><?php echo $res['ocontact']; ?></span></p>
+                                <div class="book">
+                                    <a href="bookproperty.php?id=<?php echo $res['id']; ?>">book</a>
+                                </div>
+                            </div>
+                        </div>
 
+                     <?php
+                    }
+                    if($num == 0){
+                        ?>
+                            <h1 class="text-center font-weight-bold text-success">SORRY!! NO RESULTS FOUND :)</h1>
+                        <?php
+                    }
                 }
             }
         ?>
-        <div class="result-box">
-            <div class="property-img ">
-            <img class="img-fluid" src="img/apartment.jpg" alt="">
-            </div>
-            <div><p>property name: </p>
-            <p>property address: </p></div>
-
-        </div>
+        
     </section>
 
     <?php include 'footer.php'; ?>
